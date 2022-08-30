f854edf97bfd7a347ff346cfd021dd71f1f48bde
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-30 16:39:52 -0600

    test: add test for readable attributes of a Ship

---
+++ lib/ship.rb
@@ -0,0 +1,10 @@
+class Ship
+  attr_reader :name, :size
+ 
+  def initialize(name, size)
+    @name = name
+    @size = size
+  end 
+
+
+end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,8 +5,23 @@ RSpec.describe Ship do
   it 'exists' do
     cruiser = Ship.new('Cruiser', 3)
     submarine = Ship.new('Submarine', 2)
+
     expect(cruiser).to be_a Ship
     expect(submarine).to be_a Ship
+  end
+
+  it 'has readable attributes' do
+    cruiser = Ship.new('Cruiser', 3)
+    submarine = Ship.new('Submarine', 2)
+
+    expect(cruiser.name).to eq('Cruiser')
+    expect(cruiser.size).to eq(3)
+    expect(cruiser.health).to eq(3)
+    expect(cruiser.sunk?).to eq(false)
+
+    expect(submarine.name).to eq('Submarine')
+    expect(submarine.size).to eq(2)
+    expect(submarine.sunk?).to eq(false)
   end
 
 end
